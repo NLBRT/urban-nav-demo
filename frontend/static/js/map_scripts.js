6c694c25155ebf51ea5adea131daaf28a01ff9e4
Nilabrata Sanyal <93070290+NLBRT@users.noreply.github.com>
2025-03-14 17:01:50 +0530

    Frontend changes: Styling.

--- frontend/static/js/map_scripts.js
+++ frontend/static/js/map_scripts.js
@@ -1,6 +1,24 @@
 // Initialize variables
 let activePanel = null;
 
+const greenIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+const redIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
 // Search functionality
 document.getElementById('search-btn').addEventListener('click', async () => {
     const query = document.getElementById('place-search').value;
